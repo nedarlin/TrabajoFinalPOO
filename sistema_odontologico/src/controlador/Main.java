199508b2ad999c6d3afe9a5c32acf78e7f6ea540
nedarlin <neil.linares@tecsup.edu.pe>
2026-06-13 21:27:56 -0500

    Add files via upload

---
+++ sistema_odontologico/src/controlador/Main.java
@@ -0,0 +1,24 @@
+package controlador;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        vista.vtnPrincipal ventana = new vista.vtnPrincipal();
+        ventana.setVisible(true);
+        ventana.setLocationRelativeTo(null);
+    }
+    
+}
